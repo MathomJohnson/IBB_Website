12bef4de9b662ceb695e8ed383a356e154e86828
MathomJohnson <156151500+MathomJohnson@users.noreply.github.com>
2024-05-14 23:07:42 -0500

    added home and appointments apps

--- IBB_Website/urls.py
+++ IBB_Website/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import home.views as HomeViews
+import appointments.views as AppointmentViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomeViews.homepage),
+    path('appointments/', AppointmentViews.calendar),
 ]
