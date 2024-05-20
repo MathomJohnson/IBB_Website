20636268d62475dd272c9e8a147de97663d7907f
MathomJohnson <156151500+MathomJohnson@users.noreply.github.com>
2024-05-20 16:17:17 -0500

    working on calendar

--- appointments/models.py
+++ appointments/models.py
@@ -7,3 +7,14 @@ class Appointment(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Meeting(models.Model):
+    mentor = models.CharField(max_length=200)
+    time = models.CharField(max_length=100)
+    year = models.IntegerField()
+    month = models.IntegerField()
+    day = models.IntegerField()
+    cancelled = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.mentor+", "+self.time+", "+self.year+", "+self.month+", "+self.day+", "+self.cancelled
